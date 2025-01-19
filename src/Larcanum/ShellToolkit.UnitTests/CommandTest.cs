d6476165c4225329b5386aebbb4bcfa7f3909636
Lukas Angerer <executor.dev@gmail.com>
2025-01-19 15:32:08 +0100

    Improved command parsing from single string

--- src/Larcanum/ShellToolkit.UnitTests/CommandTest.cs
+++ src/Larcanum/ShellToolkit.UnitTests/CommandTest.cs
@@ -2,6 +2,8 @@
 
 using Larcanum.ShellToolkit;
 
+using Microsoft.Extensions.Logging.Abstractions;
+
 using Xunit;
 
 namespace ShellToolkit.UnitTests;
@@ -36,5 +38,35 @@ public class CommandTest
         var psi = cmd.ToProcessStartInfo();
         psi.FileName.Should().Be("test");
         psi.ArgumentList.Should().ContainInOrder(["arg1", "arg2", "arg", "3"]);
+    }
+
+    [Fact]
+    public void Create_FromStringWithQuotedArgs_RespectsQuotes()
+    {
+        var cmd = Command.Create("echo \"Hello World\" some\\\"thing \"Foo \\\"with\\\" Bar\"");
+
+        var psi = cmd.ToProcessStartInfo();
+        psi.FileName.Should().Be("echo");
+        psi.ArgumentList.Should().ContainInOrder(["Hello World", "some\"thing", "Foo \"with\" Bar"]);
+    }
+
+    [Fact]
+    public void Create_FromStringOtherEscapeSequence_PreservesEscapeSequences()
+    {
+        var cmd = Command.Create("echo Hello\\-World \"Foo \\\\ Bar\"");
+
+        var psi = cmd.ToProcessStartInfo();
+        psi.FileName.Should().Be("echo");
+        psi.ArgumentList.Should().ContainInOrder(["Hello\\-World", "Foo \\\\ Bar"]);
+    }
+
+    [Fact]
+    public void Create_FromStringWithQuotedPath_PreservesPath()
+    {
+        var cmd = Command.Create("cmd.exe /c dir \"C:\\Program Files\\dotnet\"");
+
+        var psi = cmd.ToProcessStartInfo();
+        psi.FileName.Should().Be("cmd.exe");
+        psi.ArgumentList.Should().ContainInOrder(["/c", @"C:\Program Files\dotnet"]);
     }
 }
--- src/Larcanum/ShellToolkit/Command.cs
+++ src/Larcanum/ShellToolkit/Command.cs
@@ -1,9 +1,25 @@
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace Larcanum.ShellToolkit;
 
-public class Command : ICommand
+public partial class Command : ICommand
 {
+    /// <summary>
+    /// Matches one of two basic patterns
+    /// - Any number of non-greedy consecutive non-space characters except for a single double quote character unless
+    ///   that double quote character is immediately preceded by a back-slash, followed by any number of spaces or the
+    ///   end of the input sequence (Group 1)
+    /// - A double quote character followed by any number of non-greedy characters up until the next double quote
+    ///   character which is not immediately preceded by a back-slash, followed by any number of spaces or the end of
+    ///   the input sequence (Group 2)
+    /// </summary>
+    [GeneratedRegex(@"((?:[^""]|\\"")+?)(?:\s+|$)|""(.*?)(?<!\\)""(?:\s+|$)")]
+    private static partial Regex ArgumentExpression { get; }
+
+    [GeneratedRegex(@"\\([""])")]
+    private static partial Regex EscapeSequenceExpression { get; }
+
     public static Command Create(string cmd, IEnumerable<string> cmdArgs)
     {
         return new Command(cmd, cmdArgs);
@@ -16,7 +32,10 @@ public class Command : ICommand
 
     public static Command Create(string cmd)
     {
-        var args = cmd.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).ToList();
+        var args = ArgumentExpression
+            .Matches(cmd).Select(m => m.Groups[1].Success ? m.Groups[1].Value : m.Groups[2].Value)
+            .Select(val => EscapeSequenceExpression.Replace(val, "$1"))
+            .ToArray();
         var commandName = args[0];
         return new Command(commandName, args[1..]);
     }
